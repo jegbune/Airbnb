ca99450563fafb83bb0916457d9b82b972c4812d
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-04-21 01:21:21 +0100

    React lesson Airbnb

---
+++ src/component/Heroes.js
@@ -0,0 +1,47 @@
+import React from "react"
+
+function Heroes() {
+    return (
+        <section className="Hero-section">
+
+        <div className="heroes">
+            {/* <img src="./images/heroes/hero-grid.png"  classname= "hero-grid" alt= "Hero-grid" /> */}
+            <img src="./images/heroes/hero1.png" className="hero1" alt="hero1" />
+            <img src="./images/heroes/hero2.png" className="hero2" alt="hero1" />
+            <img src="./images/heroes/hero3.png" className="hero3" alt="hero1" />
+            <img src="./images/heroes/hero4.png" className="hero4" alt="hero1" />
+            <img src="./images/heroes/hero5.png" className="hero5" alt="hero1" />
+            <img src="./images/heroes/hero6.png" className="hero6" alt="hero1" />
+            <img src="./images/heroes/hero7.png" className="hero7" alt="hero1" />
+            <img src="./images/heroes/hero8.png" className="hero8" alt="hero1" />
+            <img src="./images/heroes/hero9.png" className="hero9" alt="hero1" />
+
+            {/* <div className="col-1">
+
+            </div>
+            
+            <div className="col-2">
+
+            </div>
+            
+            <div className="col-3">
+                
+            </div>
+            
+            <div className="col-4">
+
+            </div>
+
+            <div className="col-5">
+
+            </div> */}
+        </div>
+            <h1 className="hero-header">
+                Online Experiences
+            </h1>
+            <p className="hero-text">Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
+        </section>
+    )
+}
+
+export default Heroes
---
+++ src/component/navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function NavBar() {
+    return(
+        <nav>
+            <img src= "./images/airbnb-logo.png" className="nav-logo" alt = "logo" />
+        </nav>
+    )
+}
+
+export default NavBar
