b642c24e4bf38068c7ec34d030c89c97c5efad33
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-04-24 00:31:07 +0100

    props object React

--- src/contact.js
+++ src/contact.js
@@ -1,18 +1,18 @@
 import React from "react"
 
-function Contact({img, name, Number, mail}) {
-    // console.log()
+function Contact(props) {
+    console.log(props)
     return (
       <div className="contact-card">
-          <img src={img} alt="mr-whiskerson" />
-          <h3>{name}</h3>
+          <img src={props.img} alt="mr-whiskerson" />
+          <h3>{props.name}</h3>
           <div className="info-group">
               <img src="./images/phone-icon.png" alt="phone-icon" />
-              <p>{Number}</p>
+              <p>{props.Number}</p>
           </div>
           <div className="info-group">
               <img src="./images/mail-icon.png" alt="mail-icon" />
-              <p>{mail}</p>
+              <p>{props.mail}</p>
           </div>
       </div> 
     )
