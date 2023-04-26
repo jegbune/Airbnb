2d6bbdaa93db68bfec6189b1f151a1eeb93b4cd8
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-04-26 22:36:42 +0100

    React call API

--- src/App.js
+++ src/App.js
@@ -29,30 +29,34 @@
 // import Cards from "./component/cards"
 // import Sample from "./javaScript"
 // import Contact from "./contact";
-// import Joke from "./joke.js"
+import Joke from "./joke.js"
 // // import "./style.css"
 // import "./style1.css"
+import jokeData from "./jokeData.js"
 
 function App() {
 
   // const colors = ["Red", "orange", "Yellow", "Green", "Blue", "Indigo","Violet"]
-  const colors = [
-  <h3>Red</h3>,
-  <h3>Orange</h3>,
-  <h3>Yellow</h3>,
-  <h3>Green</h3>,
-  <h3>Blue</h3>,
-  <h3>Indigo</h3>,
-  <h3>Violet</h3>
-]
+//   const colors = [
+//   <h3>Red</h3>,
+//   <h3>Orange</h3>,
+//   <h3>Yellow</h3>,
+//   <h3>Green</h3>,
+//   <h3>Blue</h3>,
+//   <h3>Indigo</h3>,
+//   <h3>Violet</h3>
+// ]
 //  const list = colors.map(function(item){
 //   return item
 // })
-  
+const jokeElements = jokeData.map(joke => {
+  return <Joke setup = {joke.setup}  punchline = {joke.punchline} />
+})
 return(
 <div>
-{colors}
-{/* <h3>{list}</h3> */}
+  {jokeElements}
+{/* {colors} */}
+{/* <h3>{list}</h3> */} 
   {/* <NavBar />
   <Heroes />
   <Cards
@@ -92,36 +96,9 @@ return(
     mail = "pumpkin@scrimba.com"
     />
   </div> */}
-{/* <div>
-<Joke
-// setup = "I got my daughter a fridge for her birthday."
-punchline="It's hard to explain puns to kleptomaniacs because they always take things literally." 
-isPun = {true}
-upvotes = {10}
-downvotes= {2}
-comments = {[{author: "", body:"", title:""}]}
-/>
-<Joke
-setup = "I got my daughter a fridge for her birthday."
-punchline="I can't wait to see her face light up when she opens it" 
-isPun = {false}
-/>
-<Joke
-setup = "How did the hacker escape the police?"
-punchline="He just ransomware!" 
-isPun = {true}
-/>
-<Joke
-setup = "Why don't pirates travel on mountain roads?"
-punchline="Scurvy" 
-isPun = {true}
-/>
-<Joke
-setup = "Why do bees stay in the hive in the winter"
-punchline="" 
-isPun = {true}
-/>
-</div> */}
+<div>
+
+</div>
 
 </div>
 
