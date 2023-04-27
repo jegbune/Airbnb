358a5b0b249e9072c42d40e75ae5a4343da27ba0
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-04-27 23:54:49 +0100

    React update

--- src/App.js
+++ src/App.js
@@ -24,40 +24,14 @@
 
 // export default App;
 
-// import NavBar from "./component/navbar"
-// import Heroes from "./component/Heroes"
-// import Cards from "./component/cards"
-// import Sample from "./javaScript"
-// import Contact from "./contact";
-import Joke from "./joke.js"
+import NavBar from "./component/navbar"
+import Heroes from "./component/Heroes"
+import Cards from "./component/cards"
 // // import "./style.css"
-// import "./style1.css"
-import jokeData from "./jokeData.js"
 
 function App() {
-
-  // const colors = ["Red", "orange", "Yellow", "Green", "Blue", "Indigo","Violet"]
-//   const colors = [
-//   <h3>Red</h3>,
-//   <h3>Orange</h3>,
-//   <h3>Yellow</h3>,
-//   <h3>Green</h3>,
-//   <h3>Blue</h3>,
-//   <h3>Indigo</h3>,
-//   <h3>Violet</h3>
-// ]
-//  const list = colors.map(function(item){
-//   return item
-// })
-const jokeElements = jokeData.map(joke => {
-  return <Joke setup = {joke.setup}  punchline = {joke.punchline} />
-})
-return(
-<div>
-  {jokeElements}
-{/* {colors} */}
-{/* <h3>{list}</h3> */} 
-  {/* <NavBar />
+<div id="root"
+  <NavBar />
   <Heroes />
   <Cards
   image = "image-12.png"
@@ -67,10 +41,9 @@ return(
   title = "Life lessons with Katie Zaferes"
   price = {136}
   />
-   <Sample /> */}
 
 
-  {/* <div className="contacts">
+  <div className="contacts">
   <Contact
     img = "./images/mr-whiskerson.jpg"
     name = "Mr. Whiskerson"
@@ -95,13 +68,9 @@ return(
     Number = "(212) CAT KING"
     mail = "pumpkin@scrimba.com"
     />
-  </div> */}
-<div>
+  </div>
 
 </div>
-
-</div>
-
 )
 
 }
