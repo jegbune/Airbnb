8c4622f99a578660332d7861acdd6d0c3f2d1757
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-04-28 02:01:45 +0100

    fetching data's from an external js file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import NavBar from "./component/navbar"
 import Heroes from "./component/Heroes"
 import Cards from "./component/cards"
-import Data from "./data.js"
+import data from "./data.js"
 import "./style.css"
 
 function App() {
+  const cardElements = data.map(card => {
+    return <Cards image = { card.image} rating = { card.rating} reviewCount = { card.reviewCount} location = { card.location} title = { card.title} price = { card.price} />
+  })
   return(
 <div> 
   <NavBar />
   <Heroes />
-  <Cards
+  {/* <Cards
   image = "image-12.png"
   rating = "5.0"
   reviewCount = {6}
   location = "USA"
   title = "Life lessons with Katie Zaferes"
   price = {136}
-  />
+  /> */}
+  {cardElements}
 
     
 
---
+++ src/data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        image : "image-12.png",
+        rating : "5.0",
+        reviewCount : 6,
+        location : "USA",
+        title : "Life lessons with Katie Zaferes",
+        price :136,
+    },
+
+    {
+        image : "wedding-photography1.png",
+        rating : "5.0",
+        reviewCount : 6,
+        location : "USA",
+        title : "Life lessons with Katie Zaferes",
+        price :136,
+    },
+
+    {
+        image : "mountain-bike.png",
+        rating : "5.0",
+        reviewCount : 6,
+        location : "USA",
+        title : "Life lessons with Katie Zaferes",
+        price :136,
+    }
+    
+]
