3068f7dc440ac71864b7d8323537d29b6086cc39
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-04-28 01:26:52 +0100

    React Lesson

--- src/App.js
+++ src/App.js
@@ -1,79 +1,27 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import NavBar from "./component/navbar"
 import Heroes from "./component/Heroes"
 import Cards from "./component/cards"
-import Contact from "./contact"
+import Data from "./data.js"
 import "./style.css"
 
 function App() {
-<div>
+  return(
+<div> 
   <NavBar />
   <Heroes />
   <Cards
   image = "image-12.png"
   rating = "5.0"
   reviewCount = {6}
-  country = "USA"
+  location = "USA"
   title = "Life lessons with Katie Zaferes"
   price = {136}
   />
 
+    
 
-  <div className="contacts">
-  <Contact
-    img = "./images/mr-whiskerson.jpg"
-    name = "Mr. Whiskerson"
-    Number = "(212) 555-1234"
-    mail = "mr.whiskaz@catnap.meow"
-  />
-  <Contact
-    img = "./images/fluffykins.jpg"
-    name = "Fluffykins"
-    Number = "(212) 555-2345"
-    mail = "fluff@me.com"
-    />
-  <Contact
-    img = "./images/felix.jpg"
-    name = "Felix"
-    Number = "(212) 555-4567"
-    mail = "thecat@hotmail.com"
-    />
-  <Contact
-    img = "./images/pumpkin.jpg"
-    name = "Pumpkin"
-    Number = "(212) CAT KING"
-    mail = "pumpkin@scrimba.com"
-    />
-  </div>
-)
 </div>
-
-
+  )
 }
 
 export default App
--- src/component/cards.js
+++ src/component/cards.js
@@ -10,7 +10,7 @@ function Cards(props) {
             <div className="card-info">
                 <div className="stat-container">
                 <img src="./images/cards/Star 1.png" className="star" alt="star" />
-                <span>{props.rating} <span className="feeds-stats">( {props.reviewCount}).{props.country}</span></span>
+                <span>{props.rating} <span className="feeds-stats">( {props.reviewCount}).{props.location}</span></span>
                 </div>
                 <p>{props.title}</p>
                 <p className="price-container"><span className="Price">From ${props.price}</span> / person</p>
